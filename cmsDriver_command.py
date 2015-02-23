27e833124933c313baec2c6c11453f417b6a8c11
Navid <navid.rad@cern.ch>
2015-02-23 13:18:23 +0100

    step1_HLT1

--- cmsDriver_command.py
+++ cmsDriver_command.py
@@ -1,6 +1,6 @@
 
 ##GENSIM
-##cmsDriver.py  Configuration/GenProduction/python/genfragment_cff.py --filein file:input.lhe  --fileout file:FSQ-RunIIFall14GS-00001.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions MCRUN2_71_V1::All --beamspot NominalCollision2015 --step GEN,SIM --magField 38T_PostLS1 --no_exec -n 147
+##cmsDriver.py  Configuration/GenProduction/python/genfragment_cff.py --filein file:input.lhe  --fileout file:FSQ-RunIIFall14GS-00001.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions MCRUN2_71_V1::All --beamspot NominalCollision2015 --step GEN,SIM --magField 38T_PostLS1 --no_exec --python_filename step0_GENSIM_cfg.py -n 147
 
 ##
 #step1 --filein dbs:/DYToMuMu_M_20_TuneCUETP8M1_13TeV_pythia8/RunIIFall14GS-MCRUN2_71_V1-v1/GEN-SIM --fileout file:JME-Fall14DR73-00005_step1.root --pileup_input dbs:/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIFall14GS-MCRUN2_71_V1-v3/GEN-SIM --mc --eventcontent RAWSIM --pileup Flat_20_50_50ns --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions MCRUN2_73_V9 --step DIGI,L1,DIGI2RAW,HLT:GRun --magField 38T_PostLS1 --python_filename /afs/cern.ch/cms/PPD/PdmV/work/McM/submit/JME-Fall14DR73-00005/JME-Fall14DR73-00005_1_cfg.py --no_exec -n 72
@@ -8,7 +8,6 @@
 
 
 cmsDriver.py step1 --filein dbs:/T2DegStop2j_300_270_GENSIM/nrad-T2DegStop2j_300_270_GENSIM-edbe023d17b99f8a8fbdf4e576e17580/USER --fileout file:T2DegStop2j_300_270_GENSIMHLT_step1.root --pileup_input dbs:/MinBias_TuneCUETP8M1_13TeV-pythia8/RunIIFall14GS-MCRUN2_71_V1-v3/GEN-SIM --mc --eventcontent RAWSIM --pileup Flat_20_50_50ns --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions MCRUN2_73_V9 --step DIGI,L1,DIGI2RAW,HLT:GRun --magField 38T_PostLS1 --python_filename step1_HLT_cfg.py --no_exec
-
 ### FROM Run2Mechanism lhe
 #Driver.py step1 --filein file:GENSIM.root --fileout file:step1.root                                                                                            --pileup_input dbs:/MinBias_TuneA2MB_13TeV-pythia8/Fall13-POSTLS162_V1-v1/GEN-SIM --mc --eventcontent RAWSIM --inputEventContent REGEN --pileup AVE_20_BX_25ns --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --datatier GEN-SIM-RAW --conditions PHYS14_25_V1 --step GEN:fixGenInfo,DIGI,L1,DIGI2RAW,HLT:GRun --magField 38T_PostLS1 -n -1 
 #
